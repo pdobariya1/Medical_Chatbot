5b1c1e383ddd367d645ab2a291ad38d652789227
Parth Dobariya <parthdobariya62@gmail.com>
2024-12-12 12:24:44 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, request
+
+from src.QAgenerator import rag_qa_chain
 
 
 app = Flask(__name__)
@@ -7,3 +9,17 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("chat.html")
+
+
+@app.route("/get", methods=["GET", "POST"])
+def chat():
+    message = request.form["msg"]
+    query = message
+    print(query)
+    response = rag_qa_chain.invoke({"input": query})
+    print("Response :", response["answer"])
+    return str(response["answer"])
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
